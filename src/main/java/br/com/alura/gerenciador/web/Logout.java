a9f5968d7575d510d355483e1460d4cecf4ccc70
Amanda Mayara <amanda.mayaraa@gmail.com>
2017-08-22 12:12:16 -0300

    Redirecionando o logout no lado do servidor

--- src/main/java/br/com/alura/gerenciador/web/Logout.java
+++ src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -3,6 +3,7 @@ package br.com.alura.gerenciador.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,7 +28,8 @@ public class Logout extends HttpServlet{
 		else
 		{
 			session.removeAttribute("usuario.logado");			
-			writer.println(AmandaPreguica.MensagemCorpoHTML("Usuario deslogado com sucesso!"));			
+			RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/paginas/logout.html");
+			dispatcher.forward(req, resp);
 		}
 		
 		
